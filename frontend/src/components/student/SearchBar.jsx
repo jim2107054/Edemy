f3a7755dbc4a4283368077643ccd607c2d18e12e
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-23 08:19:36 +0600

    create searchbar

--- frontend/src/components/student/SearchBar.jsx
+++ frontend/src/components/student/SearchBar.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-const SearchBar = () => {
+const SearchBar = ({data}) => {
+  const navigate = useNavigate();
+  const [input, setInput] = useState(data ? data : "");
+
+  const onSearchHandler = (e) => {
+    // Prevent default form submission
+    e.preventDefault();
+    if (input.trim() === "") return;
+    navigate("/course-list/" + input.toLowerCase());
+  };
   return (
-    <div>SearchBar</div>
-  )
-}
+    <form
+      onSubmit={onSearchHandler}
+      className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded"
+    >
+      <img
+        className="md:w-auto w-10 px-3"
+        src={assets.search_icon}
+        alt="search icon"
+      />
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="w-full h-full outline-none text-gray-500/80"
+        type="text"
+        placeholder="Search for courses"
+      />
+      <button
+        type="submit"
+        className="bg-blue-600 justify-center text-center rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1"
+      >
+        Search
+      </button>
+    </form>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
