a1603d261aecb82e835154b5953d62ff17d9d7f3
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-23 13:01:32 +0600

    fetch all the courses here and use in different components

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,12 +1,29 @@
-import {createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-    const value = {};
-    return (
-        <AppContext.Provider value={value}>
-            {children}
-        </AppContext.Provider>
-    );
+  const currency = import.meta.env.VITE_CURRENCY || "$";
+
+  const [allCourses, setAllCourses] = useState([]);
+
+  const navigate = useNavigate();
+
+  //fetch all courses
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  };
+
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const value = {
+    currency,
+    allCourses,
+    navigate,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
