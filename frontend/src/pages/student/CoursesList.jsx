cda9382f4f065b69d52e8ced794fac9963ee9605
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-24 20:24:28 +0600

    add Footer components to CourseList

--- frontend/src/pages/student/CoursesList.jsx
+++ frontend/src/pages/student/CoursesList.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Footer from '../../components/student/Footer'
 
 const CoursesList = () => {
   return (
-    <div>CoursesList</div>
+    <div className='flex flex-col items-center space-y-7 text-center'>
+      <h1 className='text-3xl font-bold mt-10'>Courses List</h1>
+      <Footer/>
+    </div>
   )
 }
 
