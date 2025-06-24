fc27dd1c922140c650dbef4edfce3ecbeb2d92a1
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-24 18:21:04 +0600

    create testimonialsSection

--- frontend/src/components/student/TestimonialsSection.jsx
+++ frontend/src/components/student/TestimonialsSection.jsx
@@ -16,20 +16,22 @@ const TestimonialsSection = () => {
         transformation,success, and how our <br /> platform has made a
         difference in their lives.
       </p>
-      <div>
+      <div className="grid grid-cols-auto md:px-10 sm:gap-3 md:gap-8 mt-14">
         {dummyTestimonial.map((testimonial, index) => {
           return (
-            <div key={index}>
-              <div>
-                <img src={testimonial.image} alt={testimonial.name} />
+            <div 
+            className="text-sm text-left border border-gray-500/30 rounded-lg bg-white my-3 shadow-[0px_4px_15px_0px] shadow-black/5 overflow-hidden"
+            key={index}>
+              <div className="flex items-center gap-4 px-5 py-4 bg-gray-500/10">
+                <img className="h-12 w-12 rounded-full" src={testimonial.image} alt={testimonial.name} />
                 <div>
-                  <h1>{testimonial.name}</h1>
-                  <p>{testimonial.role}</p>
+                  <h1 className="text-lg font-medium text-gray-800">{testimonial.name}</h1>
+                  <p className="text-gray-800/80">{testimonial.role}</p>
                 </div>
-
-                {/*---show star rating---*/}
-                <div>
-                  <div className="flex">
+              </div>
+              {/*---show star rating---*/}
+                <div className="px-5 pt-1 pb-7">
+                  <div className="flex gap-0.5">
                     {[...Array(5)].map((_, i) => {
                       const type = getStarType(testimonial.rating, i);
                       let src;
@@ -39,7 +41,7 @@ const TestimonialsSection = () => {
 
                       return (
                         <img
-                          className="w-3.5 h-3.5"
+                          className="w-4 h-4"
                           key={i}
                           src={src}
                           alt={testimonial.rating + " star rating"}
@@ -47,8 +49,9 @@ const TestimonialsSection = () => {
                       );
                     })}
                   </div>
+                  <p className="text-gray-600 mt-3">{testimonial.feedback}</p>
+                  <a href="#" className="text-blue-500 font-medium mt-1 underline">Read more...</a>
                 </div>
-              </div>
             </div>
           );
         })}
