7b2fdf58a96aa93ffd356234e215db476f62fbe9
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-24 22:38:21 +0600

    created footer component.

--- frontend/src/components/student/Footer.jsx
+++ frontend/src/components/student/Footer.jsx
@@ -10,10 +10,12 @@ const Footer = () => {
   return (
     <footer className='bg-gray-900 md:px-36 text-left w-full mt-10'>
       <div className='flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30'>
-        <div>
+        {/*------------ First Div ------------*/}
+        <div className='flex flex-col items-center md:items-start w-full'>
           <img src={assets.logo_dark} alt="logo" />
-          <p className='mt-6 text-center md:text-left text-sm text-white/80'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit architecto vel totam labore aliquid aliquam voluptas quae quaerat vitae eius.</p>
+          <p className='mt-6 px-5 sm:px-28 md:px-0 text-center md:text-left text-sm text-white/80'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit architecto vel totam labore aliquid aliquam voluptas quae quaerat vitae eius.</p>
         </div>
+        {/*------------ Second Div ------------*/}
         <div className='flex flex-col md:items-start items-center w-full'>
           <h2 className='font-semibold text-white mb-5'>Company</h2>
           <ul className='flex text-center md:text-start flex-col w-full justify-between text-sm text-white/80 md:space-y-2'>
@@ -24,7 +26,18 @@ const Footer = () => {
             <Link className='hover:text-yellow-500 transition-all duration-300' to={'/privacy'} onClick={()=> {handleSmoothScroll}}>Privacy Policy</Link>
           </ul>
         </div>
-        <div></div>
+        {/*------------ Third Div ------------*/}
+        <div className='hidden md:flex flex-col items-start w-full'>
+          <h2 className='font-semibold text-white mb-5'>Subscribe to our newsletter</h2>
+          <p className='text-sm text-white/80'>The latest news, articles, and resources, sent to your inbox weekly.</p>
+          { /*------------ User email and subscribe ------------*/}
+          <div className='flex items-center gap-2 pt-4'>
+            <input
+            className='border border-gray-600/30 bg-gray-800 text-gray-500 placeholder-gray-500 outline-none w-64 h-10 rounded-md px-2 text-sm'
+             type="email" placeholder='Enter your email' />
+            <button className='bg-blue-600 w-24 hover:scale-105 h-10 text-white rounded-md'>Subscribe</button>
+          </div>
+        </div>
       </div>
       {/*-------- show the copy@right message--------*/}
       <p className='text-xs md:text-sm text-white/70 text-center py-2'>Copyright 2025 © Edemy. All Right Reserved.</p>
