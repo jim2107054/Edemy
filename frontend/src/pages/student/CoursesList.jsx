480ece9f5f1d718dddfc09be96430e5d4adc4c37
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-25 08:35:05 +0600

    add filter functionality to courselist page

--- frontend/src/pages/student/CoursesList.jsx
+++ frontend/src/pages/student/CoursesList.jsx
@@ -1,15 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Footer from "../../components/student/Footer";
 import { AppContext } from "./../../context/AppContext";
 import SearchBar from "./../../components/student/SearchBar";
 import { useParams } from "react-router-dom";
 import CourseCard from "./../../components/student/CourseCard";
+import { assets } from "../../assets/assets";
 
 const CoursesList = () => {
   const { navigate, allCourses } = useContext(AppContext);
 
   //we will get input from url and pass it to SearchBar component
-  const { inputParameter } = useParams();
+  const { input } = useParams();
+
+  //Filter courses based on input
+  const [filteredCourses, setFilteredCourses] = useState([]);
+  useEffect(() => {
+    if (allCourses.length > 0) {
+      const tempcourses = allCourses.slice();
+
+      if (input) {
+        const searchTerm = input.toLowerCase();
+        const filtered = tempcourses.filter(
+          (course) =>
+            course.courseTitle.toLowerCase().includes(searchTerm) ||
+            course.courseDescription.toLowerCase().includes(searchTerm)
+        );
+        setFilteredCourses(filtered);
+      } else {
+        setFilteredCourses(tempcourses);
+      }
+    }
+  }, [allCourses, input]);
   return (
     <>
       <div className="relative md:px-36 px-8 pt-20 text-left">
@@ -30,23 +51,45 @@ const CoursesList = () => {
             </p>
           </div>
           {/*-----------Right Group(SearchBar component)----------*/}
-          <SearchBar data={inputParameter} />
+          <SearchBar data={input} />
         </div>
+        {/*-------Filtered input and cross icon to remove filter-------*/}
+        {input && (
+          <div className="inline-flex items-center gap-4 px-4 py-2 border mt-8 -mb-8 text-gray-600">
+            <p>{input}</p>
+            <img
+              onClick={() => {navigate("/course-list"); scrollTo(0, 0);input("")}}
+              src={assets.cross_icon}
+              alt=""
+              className="cursor-pointer text-gray-700 font-medium"
+            />
+          </div>
+        )}
         {/*-------Add course card-------*/}
-        <div className="grid grid-cols-auto px-4 md:px-0 md:my-10 my-5 gap-5 md:gap-y-7">
-          {allCourses.map((course, index) => (
-            <CourseCard key={index} course={course} />
-          ))}
-        </div>
+        {filteredCourses.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0">
+            {filteredCourses.map((course, index) => (
+              <CourseCard key={index} course={course} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex items-center justify-center my-5">
+            <h2 className="text-center text-gray-600">
+              No courses found for "{input}"
+            </h2>
+          </div>
+        )}
         {/*-----Show All Button---------*/}
-        <div className="flex justify-center pb-32 items-center">
-          <button
-            className="text-gray-600 border border-gray-500/40 px-10 py-3 rounded"
-            onClick={() => scrollTo(0, 0)}
-          >
-            Load more
-          </button>
-        </div>
+        {filteredCourses.length > 0 && (
+          <div className="flex justify-center pb-32 items-center">
+            <button
+              className="text-gray-600 border border-gray-500/40 px-10 py-3 rounded"
+              onClick={() => scrollTo(0, 0)}
+            >
+              Load more
+            </button>
+          </div>
+        )}
       </div>
       <Footer />
     </>
