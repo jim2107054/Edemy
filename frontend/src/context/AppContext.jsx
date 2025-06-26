3a1d17d365f64b918cb0367a4d35470346de4b5b
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-26 18:48:34 +0600

    calculate no of lectures, course duration, chapter duration

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext();
 
@@ -12,17 +13,45 @@ export const AppContextProvider = ({ children }) => {
   const [isEducator, setIsEducator] = useState(true);
 
   //function to calculate average rating of a course
-  const calculateRating = (course)=>{
-    if(course.courseRatings.length===0) return 0;
+  const calculateRating = (course) => {
+    if (course.courseRatings.length === 0) return 0;
     //if we have ratings, calculate the average
     let totalRating = 0;
-    course.courseRatings.forEach((cRating)=>{
+    course.courseRatings.forEach((cRating) => {
       totalRating += cRating.rating;
-    })
-    return (totalRating/course.courseRatings.length).toFixed(1)
-  }
+    });
+    return (totalRating / course.courseRatings.length).toFixed(1);
+  };
 
-  // add the star rating logic
+  //Function to calculate course chapter time
+  const calculateChapterTime = (chapter) => {
+    let time = 0;
+    chapter.chapterContent.map((lecture) =>
+      time += lecture.lectureDuration);
+    return humanizeDuration(time * 60 *1000, { units: ["h", "m"] });
+  };
+
+  //Function to calculate course duration
+  const calculateCourseDuration = (course) => {
+    let time = 0;
+    course.courseContent.map((chapter) =>
+      chapter.chapterContent.map((lecture) => (time += lecture.lectureDuration))
+    );
+    console.log("time", time);
+    return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+  };
+
+  // Function to calculate No of lectures in the course
+  const calculateNoOfLectures = (course) => {
+    let totalLectures = 0;
+    course.courseContent.forEach((chapter) => {
+      if (Array.isArray(chapter.chapterContent)) {
+        totalLectures += chapter.chapterContent.length;
+      }
+    });
+    return totalLectures;
+  };
+
   // Helper to determine star type: full, half, or empty
   const getStarType = (rating, index) => {
     // console.log("rating and index", rating, index);
@@ -48,6 +77,9 @@ export const AppContextProvider = ({ children }) => {
     isEducator,
     setIsEducator,
     getStarType,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateNoOfLectures,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
