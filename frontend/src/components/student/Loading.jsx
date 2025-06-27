65111e64b4fd9ce30db99934473ca59c64157a8c
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-27 12:23:12 +0600

    add loading animation

--- frontend/src/components/student/Loading.jsx
+++ frontend/src/components/student/Loading.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="animate-spin rounded-full w-16 sm:w-20 aspect-square border-4 border-gray-300 border-t-4 border-t-blue-500"></div>
+      <div className="text-center font-medium text-blue-500 mt-4">Loading...</div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
