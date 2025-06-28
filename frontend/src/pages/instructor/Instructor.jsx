8d52436f2602e00a5bb7f0cbe282ff78e598751c
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-28 18:56:17 +0600

    add sidebar to instructor page

--- frontend/src/pages/instructor/Instructor.jsx
+++ frontend/src/pages/instructor/Instructor.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Instructor/Navbar";
+import Sidebar from "../../components/Instructor/Sidebar";
 
 const Instructor = () => {
   return (
     <div className="text-default min-h-screen bg-white">
       <Navbar />
-      <div>{<Outlet />}</div>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1">{<Outlet />}</div>
+      </div>
     </div>
   );
 };
