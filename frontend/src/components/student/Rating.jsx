597f6ea512119e11271b790132beb58bc85c4264
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-28 16:09:33 +0600

    create user rating system

--- frontend/src/components/student/Rating.jsx
+++ frontend/src/components/student/Rating.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Rating = () => {
+const Rating = ({initialRating,onRate}) => {
+  const [rating, setRating] = useState(initialRating || 0); // Assuming initialRating is defined somewhere in your component or props
+
+  const handleRating = (value) =>{
+    setRating(value)
+    if(onRate){
+      onRate(value);
+    }
+  }
+
+  // Render stars based on the rating
+  useEffect(()=>{
+    if(initialRating){
+      setRating(initialRating);
+    }
+  },[initialRating])
+
   return (
-    <div>Rating</div>
+    <div>
+      {
+        Array.from({length:5},(_,index)=>{
+          const startValue = index +1;
+          return (
+            <span
+            onClick={() => handleRating(startValue)}
+            key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${startValue<=rating?"text-yellow-500":"text-gray-400"}`}>
+              &#9733;
+            </span>
+          )
+        })
+      }
+    </div>
   )
 }
 
