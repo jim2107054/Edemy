b75d765a3e52808e424bc7012ac561cae1d0b4f5
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-28 18:22:48 +0600

    create navbar for instructor

--- frontend/src/components/Instructor/Navbar.jsx
+++ frontend/src/components/Instructor/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { assets, dummyEducatorData } from "../../assets/assets";
+import { UserButton, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const educatorData = dummyEducatorData;
+  const {user} = useUser()
   return (
-    <div>Ins. Navbar</div>
-  )
-}
+    <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3">
+      <Link to="/">
+        <img src={assets.logo} alt="Logo" className="w-28 lg:w-32" />
+      </Link>
+      <div className="flex items-center gap-5 text-gray-600 relative">
+        <p>Hi! {user?user.fullName:"Developers"}</p>
+        {
+          user ? <UserButton/>: <img src={assets.profile_img} className="w-10 h-10 rounded-full" />
+        }
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
