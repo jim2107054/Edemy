12a8a85bde2cfc3fd7753d46778b4953a8acf8f6
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-29 18:31:50 +0600

    add course title, description, price, thumbnail,discount

--- frontend/src/pages/instructor/AddCourse.jsx
+++ frontend/src/pages/instructor/AddCourse.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import uniqid from "uniqid"; // For generating unique IDs, for each courses
 import Quill from "quill"; // For rich text editor
+import { assets } from "./../../assets/assets";
 
 const AddCourse = () => {
   const quillRef = useRef(null); // Reference to the Quill editor instance
@@ -31,7 +32,7 @@ const AddCourse = () => {
 
   return (
     <div className="h-screen overflow-scroll flex flex-col items-start justify-between md:p-8 md:pb-0 pt-8 pb-0">
-      <form>
+      <form className="flex p-5 md:p-0 flex-col gap-4 w-full max-w-md text-gray-500">
         {/*-------Course Title-------*/}
         <div className="flex flex-col gap-1">
           <p>Course Title</p>
@@ -48,6 +49,87 @@ const AddCourse = () => {
         <div className="flex flex-col gap-1 ">
           <p>Course Description</p>
           <div ref={editorRef}></div>
+        </div>
+        {/*-------Course Price and Thumbnail-------*/}
+        <div className="flex items-center justify-between flex-wrap">
+          <div>
+            <p>Course Price</p>
+            <input
+              value={coursePrice}
+              onChange={(e) => setCoursePrice(e.target.value)}
+              placeholder="0"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500"
+              required
+              type="number"
+            />
+          </div>
+          <div className="flex flex-col md:flex-row items-center gap-3">
+            <p>Course Thumbnail</p>
+            <label className="flex items-center gap-3" htmlFor="thumbnailImage">
+              <img
+                className="p-3 bg-blue-500 rounded"
+                src={assets.file_upload_icon}
+                alt=""
+              />
+              <input
+                onChange={(e) => setImage(e.target.files[0])}
+                accept="image/*"
+                hidden
+                type="file"
+                id="thumanailImage"
+              />
+              <img
+                className="max-h-10"
+                src={image ? URL.createObjectURL(image) : ""}
+                alt=""
+              />
+            </label>
+          </div>
+        </div>
+        {/*-------Course discount-------*/}
+        <div className="flex flex-col gap-1">
+          <p>Discount %</p>
+          <input
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500"
+            value={discount}
+            onChange={(e) => setDiscount(e.target.value)}
+            placeholder="0"
+            max={100}
+            min={0}
+            required
+            type="number"
+          />
+        </div>
+        {/*---------Adding Chapters and Lectures---------*/}
+        <div>
+          {chapters.map((chapter, chapterIndex) => (
+            <div key={chapterIndex} className="bg-white border rounded-lg mb-4">
+              <div className="flex justify-between items-center p-4 border-b">
+                <div className="flex items-center">
+                  <img
+                    className={`mr-2 cursor-pointer transition-all ${
+                      chapter.collapsed && "-rotate-90"
+                    }`}
+                    src={assets.dropdown_icon}
+                    width={14}
+                    alt=""
+                  />
+                  <span className="font-medium">
+                    {chapterIndex + 1}
+                    {" " + chapter.chapterTitle}
+                  </span>
+                </div>
+                <span className="text-gray-500">
+                  {chapter.chapterContent.length} Lectures
+                </span>
+                <img
+                  className="cursor-pointer"
+                  src={assets.cross_icon}
+                  alt=""
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </form>
     </div>
