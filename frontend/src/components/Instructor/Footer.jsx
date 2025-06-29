c0f85a131224450bd123e4aecd37556741d4da9b
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-29 07:59:09 +0600

    create educator footer component

--- frontend/src/components/Instructor/Footer.jsx
+++ frontend/src/components/Instructor/Footer.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div>Ins. Footer</div>
-  )
-}
+    <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
+      {/*--------left side of footer----------*/}
+      <div className="flex items-center gap-4">
+        <img className="hidden md:block w-20" src={assets.logo} alt="Logo" />
+        <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
+        <p className="py-4 text-center text-xs md:text-sm text-gray-500">
+          Copyright 2025@Edemy. All Right Reserved.
+        </p>
+      </div>
+      {/*--------right side of footer----------*/}
+      <div className="flex items-center gap-3 max-md:mt-4">
+        <a href="#">
+          <img
+            className="hover:bg-blue-400 rounded-full hover:shadow-sm hover:shadow-cyan-600"
+            src={assets.facebook_icon}
+            alt="facebook"
+          />
+        </a>
+        <a href="#">
+          <img
+            className="hover:bg-blue-400 rounded-full hover:shadow-sm hover:shadow-cyan-600"
+            src={assets.twitter_icon}
+            alt="twitter"
+          />
+        </a>
+        <a href="#">
+          <img
+            className="hover:bg-blue-400 rounded-full hover:shadow-sm hover:shadow-cyan-600"
+            src={assets.instagram_icon}
+            alt="instagram"
+          />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
