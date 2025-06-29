529ef479f790d5272b3213f95594d88a15942d89
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-29 07:59:41 +0600

    add the Footer component to the Instructor page

--- frontend/src/pages/instructor/Instructor.jsx
+++ frontend/src/pages/instructor/Instructor.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Instructor/Navbar";
 import Sidebar from "../../components/Instructor/Sidebar";
+import Footer from "../../components/Instructor/Footer";
 
 const Instructor = () => {
   return (
@@ -11,6 +12,7 @@ const Instructor = () => {
         <Sidebar />
         <div className="flex-1">{<Outlet />}</div>
       </div>
+       <Footer/>
     </div>
   );
 };
